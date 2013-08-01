3ac4d801b91a067bc0efc364cde93057a71c8ddb
Dan Taylor <danieltaylor1977@gmail.com>
2013-08-01 20:59:00 +0000

    index.html now used

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var f = fs.readFileSync('~/bitstarter/index.html')
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(f);
 });
 
 var port = process.env.PORT || 5000;
