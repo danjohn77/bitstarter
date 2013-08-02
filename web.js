912ad6e2865bcda45b700eb31b30ce0796deffb5
Dan Taylor <danieltaylor1977@gmail.com>
2013-08-02 20:16:43 +0000

    File changed to read index

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = new buffer();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Mr Jones');
+  response.send(buffer(fs.readFileSync('~/bitstarter/index.html')));
 });
 
 var port = process.env.PORT || 5000;
